f0ce65d2a03a6fc2f8bbdb921c416ab1ed09f5af
rysun <rysun@qq.com>
2016-05-13 23:14:28 +0800

    init

--- src/test/java/com/sunruyi/stupid/trader/application/ApplicationTest.java
+++ src/test/java/com/sunruyi/stupid/trader/application/ApplicationTest.java
@@ -1,5 +1,7 @@
 package com.sunruyi.stupid.trader.application;
 
+import com.sunruyi.stupid.trader.Price;
+import com.sunruyi.stupid.trader.PriceSequence;
 import com.sunruyi.stupid.trader.Transactions;
 import com.sunruyi.stupid.trader.operation.Buy;
 import com.sunruyi.stupid.trader.operation.Pass;
@@ -26,7 +28,8 @@ public class ApplicationTest {
      */
     @Test
     public void should_buy_at_start_sell_at_end() {
-        Transactions transactions = new Transactions(new Buy(), new Pass(), new Pass(), new Pass(), new Sell());
+        Price[] prices = {new Price("1"), new Price("2"), new Price("3"), new Price("4"), new Price("5")};
+        Transactions transactions = calculate(prices);
         assertThat(transactions.print(), is("[Buy,Pass,Pass,Pass,Sell]"));
     }
 
@@ -42,7 +45,14 @@ public class ApplicationTest {
      */
     @Test
     public void should_pass_all_the_day() {
-        assertThat("ok", is("ok"));
+        Price[] prices = {new Price("5"), new Price("4"), new Price("3"), new Price("2"), new Price("1")};
+        Transactions transactions = calculate(prices);
+        assertThat(transactions.print(), is("[Pass,Pass,Pass,Pass,Pass]"));
+    }
+
+    private Transactions calculate(Price[] prices) {
+        PriceSequence priceSequence = new PriceSequence(prices);
+        return new Transactions(new Buy(), new Pass(), new Pass(), new Pass(), new Sell());
     }
 
     /**
@@ -55,7 +65,6 @@ public class ApplicationTest {
      */
     @Test
     public void should_buy_at_start_sell_at_top() {
-        assertThat("ok", is("ok"));
     }
 
     /**
@@ -68,6 +77,5 @@ public class ApplicationTest {
      */
     @Test
     public void should_buy_at_bottom_sell_at_end() {
-        assertThat("ok", is("ok"));
     }
 }
